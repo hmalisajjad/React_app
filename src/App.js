105571a0c59a08e8b5541ec60679f493ba4694cf
Hafiz Muhammad Ali Sajjad <70126786+hmalisajjad@users.noreply.github.com>
2023-08-04 12:38:41 +0200

    adding material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import "./App.css";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="center">
+      <h1>Form </h1> <br />
+      <TextField id="credientials" label="Email" /> <br />
+      <TextField id="credientials" label="Password" /> <br />
+      <Button variant="contained" color="secondary">
+        Form Submit
+      </Button>
+    </div>
+  );
 }
 
 export default App;
