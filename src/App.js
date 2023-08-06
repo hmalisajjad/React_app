b4fb7c6eed01d6749711c4ae4059d6559cccfcc6
Hafiz Muhammad Ali Sajjad <70126786+hmalisajjad@users.noreply.github.com>
2023-08-06 18:57:26 +0200

    form add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
 
 function App() {
+  const [todos, setTodos] = useState(["testing"]);
+  const [text, setText] = useState("");
+
+  const addText = () => {};
+
+  const updateText = () => {};
+
+  const deleteText = () => {};
+
+  const deleteAllText = () => {};
+
   return (
-    <div className="center">
-      <h1>Form </h1> <br />
-      <TextField id="credientials" label="Email" /> <br />
-      <TextField id="credientials" label="Password" /> <br />
-      <Button variant="contained" color="secondary">
-        Form Submit
-      </Button>
+    <div>
+      <h1 className="header">DATA APP</h1>
+      <form>
+        <Paper elevation={5} className="form">
+          <TextField
+            className="textField"
+            label="enterData"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <br />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            onClick={addText}
+          >
+            ADD
+          </Button>
+          <Button variant="contained" color="primary" onClick={deleteAllText}>
+            DELETE ALL
+          </Button>
+        </Paper>
+      </form>
+      {todos.map((data, i) => {
+        return (
+          <p>
+            {i + 1}. {data}
+          </p>
+        );
+      })}
     </div>
   );
 }
